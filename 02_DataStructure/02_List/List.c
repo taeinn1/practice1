bc227af66d3b8fda77f39545c71d435c54aa9263
taeinn1 <woawoa1214@gmail.com>
2026-02-15 15:48:26 +0900

    List 2

--- 02_DataStructure/02_List/List.c
+++ 02_DataStructure/02_List/List.c
@@ -13,6 +13,21 @@ static ListNode* cln(LstElement *e) {
 	return p;
 }
 
+ListNode* fpren(List* list, ListNode* next) {
+	ListNode* p, *r;   //값저장안하니 말록필요없음
+	p = list->head;
+	r = NULL;    //head가 next일경우 대비
+	while (p!=NULL && p!=next) { //구조체끼리는 대입연산만 가능. (+, == 불가)
+		r = p;
+		p = p->next;
+	}
+	if (p) {
+		return r;
+	}
+	// p가 끝이어도(next를 못찾아도)r을 리턴해야함 (이거하지마_> return NULL;
+	//왜냐. 그럼 head가 next인거랑 똑같은 결과라서.
+}
+
 
 
 
@@ -62,7 +77,7 @@ int LstPushBack(List* list, const LstElement* e) {
 }
 
 int LstGetEntry(List* list, int pos, LstElement* e) {
-	ListNode* p = cln(e);  //값저장할거아님 = 할당받을 필요X
+	ListNode* p; // = cln(e);  //값저장할거아님 = 할당받을 필요X
 	p = list->head;              //= 그냥 ListNode* p=list->head
 	int num = 0;
 	if (pos >= 0)
@@ -98,3 +113,120 @@ int LstInsertBefore(List* list, ListNode* next, const LstElement* e) {
 		return 0;
 	}
 }
+
+
+int LstInsertBefore(List* list, ListNode* next, const LstElement* e) {
+	if (list && next && list->head) {
+		ListNode* p;
+		if (p) {
+			p = fpren(list, next);
+			if (p) {
+				if (p != list->tail)
+					return 0;
+				return LstInsertAfter(list, p, e);
+				/*위 한줄 대신 썼던 것
+				if (p != list->tail) {
+					if (r) {
+						p->next = r;
+						r->next = next;
+						return 1;
+					}
+					return 0;
+				}*/
+			}
+			else {
+				return LstPushFront(list, e);
+			}
+		}
+	}
+	return 0;
+}
+
+int LstInsertAfter(List* list, ListNode* previous, const LstElement* e) {
+	//ListNode* p = fpren(list, previous);
+	//위의 애는 틀림. 왜냐면 이미 삽입에 필요한 노드(앞노드. previous)가 주어졌는데 또찾는셈이라. 심지어 앞의 앞노드임
+	ListNode* r = cln(e), * p;
+	//만약 아래서 실패시 낭비됨. if 성공시로 옮기면 굿
+
+	if (list && previous) {
+		//위는 리스트포인터가 할당되었는지만 확인할뿐, 리스트 내부에 previous가 존재하는지는 확인하지 않음
+		p = list->head;
+		while (p && p != previous) {
+			p = p->next;
+		}
+		if (p) {
+			if (r) {
+				r->next = previous->next;
+				previous->next = r;
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+
+int LstInsert(List* list, int pos, const LstElement* e) {
+	if (list && list->head &&pos!=0) {
+		ListNode* p=list->head;
+		int i = 0;
+		while (i != pos && p) {
+			p = p->next;
+			i++;
+		}
+		if (i = pos) {
+			LstInsertAfter(list, p, e);
+			return 1;
+		}
+		else {
+			return 0;
+		}
+
+	}
+}
+
+void LstPrint(List* list, PrintFunc print) {
+	ListNode* p = list->head;
+	while (p) {
+		print(&(p->element));
+		p = p->next;
+	}
+}
+
+int LstDelete(List* list, ListNode* node) {
+	if (list && node && list->head) {
+		ListNode* p = fpren(list, node);
+		if (p == NULL) {
+			list->head = list->tail = 0;
+			free(node);
+			return 1;
+		}
+		else if (p == list->tail) {
+			return 0;
+		}
+		if (p->next == list->tail) {
+			list->tail = p;
+		}
+
+		p->next = node->next;
+		free(node);
+		return 1;
+	}
+	return 0;
+}
+
+
+void LstClear(List* list) {
+	if (list && list->head) {
+		ListNode* p;
+		while (list->head) {
+			p = list->head->next;
+			free(list->head);
+			list->head = p;
+		}
+	}
+	//리스트 내부를 free하려면 노드 하나하나를 건너가며 free해줘야하는구나.
+	//하긴 노드 하나하나는 개별적인 독립체고 연결만 된거니까
+}
+
+ListNode* LstFindNode(List* list, const LstElement* e, CompareFunc compare);
